e7d82ff4ffb034d77f2944e6246f0ebd979cffa2
YangShichang <iyaya@iyayadeMacBook-Pro.local>
2014-08-15 17:40:15 +0800

    add file

---
+++ ImageResize/ImageResize/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ImageResize
+//
+//  Created by iyaya on 14-8-15.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
